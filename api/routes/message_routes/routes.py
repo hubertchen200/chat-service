badb253f0a94d5d437ab44236c7cefa034e5c0ed
Hubert Chen <hubertchen@Huberts-iMac.hsd1.nj.comcast.net>
2024-07-13 11:24:23 -0400

    refacter token

--- api/routes/message_routes/routes.py
+++ api/routes/message_routes/routes.py
@@ -1,18 +1,15 @@
 from flask import request, jsonify
 from api.chat import get_message, send_message, delete_message
 from . import message_bp
+from api.jwt_token.my_jwt import check_token
 from api.jwt_token.my_jwt import jwt_decode
 
 
 @message_bp.route('/message', methods = ['GET', 'POST', 'DELETE'])
 def my_message():
-    token = request.headers.get('Authorization')
-    token = token.replace("Bearer ", "")
-    payload = jwt_decode(token)
-    if payload == "TOKEN_EXPIRED":
-        return jsonify({'error': "TOKEN_EXPIRED"})
-    if payload == "INVALID_TOKEN":
-        return jsonify({'error': 'INVALID_TOKEN'})
+    data, code = check_token(request.headers)
+    if code == 401:
+        return jsonify(data), code
     if request.method == 'GET':
         id = request.args.get('id', default=1, type=int)
         count = request.args.get('count', default=10, type=int)
